08ded6c96e9ccd80e9efaa17dce16f4d5c8820bd
unknown <ornitmen@gmail.com>
2020-11-05 13:00:09 +0200

    first master commit

---
+++ WOrk21/WOrk21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x;
+    double z1;
+
+    cout << "x = ";
+    cin >> x;
+    z1 = 2 * x;
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+
+    cin.get();
+    return 0;
+
+}
